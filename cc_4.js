35c2ba54236bcd1e60069bd96357fd1e31136bbb
adamyrzakanova <aidaimyrzakanova@usf.edu>
2025-02-02 22:39:19 -0500

    Task 2 - Sales Report.

--- cc_4.js
+++ cc_4.js
@@ -9,3 +9,16 @@ if (purchaseAmount > 100) {
 
 // Log final amount
 console.log(`Final amount after discount: $${purchaseAmount}`);
+
+// Task 2
+// Declare array sales 
+let sales = [120, 85, 200, 150, 90];
+
+// Use for loop to calculate total sales
+forloopTotal = 0;
+for (let i = 0; i < sales.length; i++) {
+    forloopTotal += sales[i];
+}
+
+// Log total sales
+console.log(`Total sales: $${forloopTotal}`);
