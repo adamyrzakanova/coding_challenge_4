3a7f4961e49df89dae06bdd961c9aa681a6c9640
adamyrzakanova <aidaimyrzakanova@usf.edu>
2025-02-02 22:37:52 -0500

    Task 1 - Customer Discounts.

---
+++ cc_4.js
@@ -0,0 +1,11 @@
+// Task 1 
+// Declare a variable purchaseAmount with a value 
+let purchaseAmount = 1000;
+
+// if statement to apply discount >100 
+if (purchaseAmount > 100) {
+    purchaseAmount = purchaseAmount - 100;
+}
+
+// Log final amount
+console.log(`Final amount after discount: $${purchaseAmount}`);
